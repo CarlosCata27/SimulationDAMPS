1644d1281dc6af68bb9bea955dd0ea56d3dc4500
Enrique Velasco <velascoenrique396@gmail.com>
2021-10-17 19:43:14 -0500

    bloqueo de llamadas

--- Simulation.py
+++ Simulation.py
@@ -65,7 +65,7 @@ for x in range(Filas):
     Circuitos.append([])
     for y in range(Columnas):
         Circuitos[x].append([])
-        for z in range(3):
+        for z in range(4):
            Circuitos[x][y].append(0)
 
 #Llenar el numero correspondiente de slot
@@ -75,24 +75,46 @@ for i in range(Filas):
         Circuitos[i][j][0]=Llenador
         Llenador +=1
 #Pasar la lista a una matriz tridimensional       
-Circuitos = np.array(Circuitos).reshape(Filas,Columnas,3)
+Circuitos = np.array(Circuitos).reshape(Filas,Columnas,4)
 
 
 Nllamada=1
 Segundero = (Nllamada*Ritmoarribo)+(mu*26)
 Llenador=1
+Pllamadas = 100
 while (Segundero<=hp and Nllamada<=N_ev and Llenador <=N_ev):
     #time.sleep(Ritmoarribo)
    
     for i in range(Filas):
         for j in range(Columnas):
             if(Circuitos[i][j][2]==0):
-                Circuitos[i][j][1]=Llenador
-                Circuitos[i][j][2]=H
-                Llenador +=1
+
+                #Asignación de la llamada
+                #Proceso de rechazo o admisión (Método probabilístico utilizando el porcetaje de bloque de Erlang B)
+                #nB = 0.01 -> 1% [Se pierden 1 de cada 100 llamadas]
+                time.sleep(1)
+                Pllamadas -= 1
+                if Pllamadas > 0:
+                    Circuitos[i][j][1]=Llenador #Indicador del número de llamada
+                    Circuitos[i][j][2]=H #Duración de la llamada
+                    Circuitos[i][j][3] = 1 #Llamada aceptada
+                    Llenador +=1
+                    print("Accepted")
+                    print(Circuitos[i][j])
+                else:
+                    Circuitos[i][j][1]=Llenador #Indicador del número de llamada
+                    Circuitos[i][j][2]=0 #Duración de la llamada
+                    Circuitos[i][j][3] = 0 #Llamada aceptada
+                    print("No Accepted")
+                    print(Circuitos[i][j])
+
+                    #Volvemos a las 100 llamadas
+                    Pllamadas = 100
+                    
             else:
                 Circuitos[i][j][2]-=1
-    print(Circuitos)
+            
+    # print(Circuitos)
     #time.sleep(mu)
     
     Nllamada +=1
